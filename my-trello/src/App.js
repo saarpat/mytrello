b6ef8558367eb68bcdc4f50a2b150386d25a6f45
pazSaar <pazsaar@gmail.com>
2022-04-28 12:54:28 +0300

    new files added

---
+++ my-trello/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <button>Add another List</button>
+    </div>
+  );
+}
+
+export default App;
